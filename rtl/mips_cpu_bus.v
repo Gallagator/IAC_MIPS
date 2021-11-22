4444f276ab1ff22bf265015c2fca2941f92710f8
Liam Gallagher <liam.d.gallagher@protonmail.com>
2021-11-22 16:44:48 +0000

    Update ir apropriately. Wait for bus to return value.

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -16,8 +16,90 @@ module mips_cpu_bus(
     output logic[3:0] byteenable,
     input logic[31:0] readdata
 );
-    logic[31: 0] pc;
+    typedef enum logic[5:0] {
+        OPCODE_RTYPE = 6'd0
+    } opcode_t;
     
+    typedef enum logic[5:0] {
+        FUNCT_ADDU = 6'b10_0001;
+    } funct_t;
+
+    typedef enum logic[2:0] {
+        STATE_FETCH,
+        STATE_EXECUTE,
+        STATE_MEMORY,
+        STATE_WRITEBACK
+    } state_t;
+
+    /* Program Counter, instruction register, state */
+    logic[31: 0] pc, pc_next;
+    logic[31: 0] ir, effective_ir;
+    state_t state;
+
+    /* R-type */ 
+    opcode_t opcode;
+    logic[4:0] rs, rt, rd;
+    logic[4:0] shamnt;
+    funct_t func;
+
+    always_comb begin
+        /* Decoding */
+        /* Grabs the instruction that has just been fetched. */
+        effective_ir = state == STATE_EXECUTE ? readdata : ir;
+        /* rtype */ 
+        opcode = effective_ir[31:26];
+        rs     = effective_ir[25:21];
+        rt     = effective_ir[20:16];
+        rd     = effective_ir[15:11];
+        shamnt = effective_ir[10:6];
+        func   = effective_ir[5:0];
+
+        /* Fetch */
+        if(state == STATE_FETCH) begin
+            address = pc << 2;
+            read = 1;
+        end
+    end
+
+    always_ff @(posedge clk) begin
+        if(reset) begin
+            pc <= 32'hBFC0_0000;
+            state <= STATE_FETCH;
+        end
+        else begin
+            case(state) begin 
+                STATE_FETCH begin
+                    pc <= pc + 1
+                    state <= STATE_EXECUTE;
+                end
+                STATE_EXECUTE begin
+                    /* Won't exit the execute state if bus hasn't been read 
+                     * from yet. Further, it won't do anything if its still
+                     * waiting */
+                    if(!waitrequest) begin
+                        ir <= readdata;
+                        case(opcode) 
+                            OPCODE_RTYPE begin
+                                case(func)
+                                    FUNCT_ADDU begin
+                                        
+                                    end 
+                                endcase 
+                            end                
+                        endcase
+                    end
+                end
+                STATE_MEMORY begin
+                   
+                end
+                STATE_WRITEBACK begin
+
+                end
+            end
+             
+        end
+    end
+
     reg_file reg_file(.clk(clk), 
                       .reset(reset), 
                       .addr_a(),
@@ -27,28 +109,6 @@ module mips_cpu_bus(
                       .data_in(),
                       .a(),
                       .b()
-    ); 
-
-    typedef enum logic[5:0] {
-        OPCODE_RTYPE = 6'd0
-    } opcode_t;
-
-    typedef enum logic[2:0] {
-        STATE_FETCH,
-        STATE_DECODE,
-        STATE_EXECUTE,
-        STATE_MEMORY,
-        STATE_WRITEBACK
-    } state_t;
-
-    always_ff @(posedge clk) begin
-        if(reset) begin
-            pc <= 32'hBFC0_0000;
-        end
-        else begin
-
-        end
-    end
-
+    );
     
 endmodule
