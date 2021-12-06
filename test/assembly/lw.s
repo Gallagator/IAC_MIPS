8ece877b025ad1933b5ac99881a07b2b2161293c
Liam Gallagher <liam.d.gallagher@protonmail.com>
2021-12-06 15:23:08 +0000

    Write lwsw testcase.

---
+++ test/assembly/lw.s
@@ -0,0 +1,12 @@
+	.text
+	.align	2
+	.globl	main
+	.ent	main
+main:
+	.set	noreorder
+    addiu $4, $0, 1234 
+    addiu $5, $0, 0x30
+    sw    $4, 10($5)
+    lw    $2, 10($5)
+    jr $0
+.end	main
