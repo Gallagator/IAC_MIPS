f407ea89d5937f0219c9d96ae915ed3bc59c7374
Liam Gallagher <liam.d.gallagher@protonmail.com>
2021-12-08 12:19:00 +0000

    add lw testcase

--- test/assembly/lw2.s
+++ test/assembly/lw2.s
@@ -1,4 +1,4 @@
-	.text
+    .text
 	.align	2
 	.globl	main
 	.ent	main
@@ -8,6 +8,9 @@ main:
     addiu $5, $0, 0x30
     sw    $4, 8($5)
     lw    $2, 8($5)
+    addu  $2, $2, $5
+    sw    $2, 4($5)
+    lw    $2, 4($5)
     jr $0
 .end	main
 
--- test/assembly/sw_3.s
+++ test/assembly/sw_3.s
@@ -6,10 +6,10 @@ main:
 	.set	noreorder
     addiu $4, $0, 1234
     addiu $5, $0, 0x30
-    sw    $4, 10($5)
-    lw    $3, 10($5)
+    sw    $4, 8($5)
+    lw    $3, 8($5)
     addu  $4, $3, $5
-    sw    $4, 10($3)
-    lw    $2, 10($3)
+    sw    $4, 8($3)
+    lw    $2, 8($3)
     jr $0
 .end	main
