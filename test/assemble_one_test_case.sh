bf7d87d96c226f9464769693c020de71eacccedd
Liam Gallagher <liam.d.gallagher@protonmail.com>
2021-12-13 14:35:04 +0000

    Improve testsuite.

--- test/assemble_one_test_case.sh
+++ test/assemble_one_test_case.sh
@@ -8,7 +8,10 @@ mips-linux-gnu-gcc -march=mips1 -mfp32 -O0 -c -EL assembly/${TESTNAME}.s -o ${TE
 # Turn object file into a raw binary. Only include the .text section (the code)
 mips-linux-gnu-objcopy -O binary -j .text ${TESTNAME}.o ${TESTNAME}.bin
 # Open in a hex editor and format for 4 bytes per line. Store this in a .hex.txt
-xxd -ps -c 4 ${TESTNAME}.bin > machine_code/${TESTNAME}.hex.txt
+BINARY=$(xxd -ps -c 4 ${TESTNAME}.bin)
+
+echo -e "@0\n${BINARY}" > machine_code/${TESTNAME}.hex.txt
 
 # Remove intermediate files
 rm ${TESTNAME}.o ${TESTNAME}.bin
+
