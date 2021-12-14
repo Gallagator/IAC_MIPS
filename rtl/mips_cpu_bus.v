2543510f8fd12d286103b9c527ee1d233473f01d
julio <Julio.Castillejo.Motta@gmail.com>
2021-12-14 21:38:17 +0000

    Fixed mem instructions

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -141,7 +141,7 @@ module mips_cpu_bus(
                     reg_file_write = 0;
                 end
                 else if(opcode == OPCODE_SW) begin
-                    write = 1; 
+                    write = 0; 
                     read = 0;
                     byteenable = 4'b1111;
                     address = alu_out;
@@ -177,6 +177,9 @@ module mips_cpu_bus(
                     OPCODE_LW : begin
                         reg_file_data_in = readdata_eb;
                     end
+                    OPCODE_SW : begin
+                        write = 1;
+                    end
                     default : begin     // LBU, LB, LHU, LH, LWL, LWR
                         reg_file_data_in = loadstore_word;
                     end
@@ -184,23 +187,6 @@ module mips_cpu_bus(
             end
         endcase
 
-    end
-
-    always @(state) begin
-        case(state)
-            STATE_FETCH: $display("----------------------------------------\nSTATE FETCH");
-            STATE_EXECUTE: $display("STATE EXEC");
-            STATE_MEMORY: $display("STATE MEMORY");
-        endcase
-    end
-
-    always @(opcode) begin
-        case(opcode)
-            OPCODE_ADDIU: $display("ADDIU");
-            OPCODE_SW: $display("STORE WORD");
-            OPCODE_LW: $display("LOAD WORD");
-            OPCODE_RTYPE: $display("JUMP REGISTER");
-        endcase
     end
 
     always @(posedge clk) begin
@@ -227,7 +213,6 @@ module mips_cpu_bus(
                 end
                 STATE_EXECUTE : begin
                     ir <= waitrequest_prev ? ir : readdata_eb;
-                    
                     if(branch_delayed == BRANCH_DELAYED) begin
                         pc <= pc_branch;
                         branch_delayed <= BRANCH_NONE;
@@ -254,7 +239,7 @@ module mips_cpu_bus(
                             end
                             else if(opcode == OPCODE_SW) begin
                                 if(!waitrequest) begin 
-                                    state <= STATE_FETCH;
+                                    state <= STATE_MEMORY;
                                 end
                             end 
                             else begin
--- test/assembly/lw/lw.s
+++ test/assembly/lw/lw.s
@@ -4,10 +4,10 @@
 	.ent	main
 main:
 	.set	noreorder
-    addiu $4, $0, 1234 
-    addiu $5, $0, 0x8
-    sw    $4, -4($5)    /* mem addr is 4 = 8 - 4 */
+    addiu $4, $0, 0x1234 
+    addiu $5, $0, 0x10
+    sw    $4, -4($5)    /* mem addr is 12 = 16 - 4 For this testcase it fails*/
     jr $0
-    lw    $2, -4($5)    /* mem addr is 4 = 8 - 4 */
+    lw    $2, -4($5)    /* mem addr is 12 = 16 - 4 */
 .end	main
 
