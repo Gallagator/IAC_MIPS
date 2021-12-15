138daaab53922d25fb86434aec0d24e87745ae10
Liam Gallagher <liam.d.gallagher@protonmail.com>
2021-12-15 15:06:33 +0000

    add lwr2

---
+++ test/assembly/lwr/lwr_2.s
@@ -0,0 +1,13 @@
+    .text
+	.align	2
+	.globl	main
+	.ent	main
+main:  
+	.set	noreorder
+    lui   $3, 0x1234
+    addiu $3, 0x5678
+    sw    $3, 0($0)
+    jr $0
+    lwr   $2, 1($0) 
+.end	main
+
