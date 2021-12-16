62846f4e5ff740563801dc89d15184caf0b3d16c
Miguel Braganca <m.pl.braganca@gmail.com>
2021-12-16 21:49:39 +0000

    jalr tests fixed

--- test/assembly/jalr/jalr2.s
+++ test/assembly/jalr/jalr2.s
@@ -4,24 +4,24 @@
 	.ent	main
 main:
     .set	noreorder
-    jalr next
+    addiu $3, $3, 24
+    jalr $8, $3
     addiu $2, $2, 1
     jr $0
     addiu $2, $2, 2
-
-next:
+    
     addiu $2, $2, 4
-    jalr next2, $14
+    addiu $3, $3, 52
+    jalr $14, $3
     addiu $2, $2, 8
     addiu $2, $2, 16
     jr $0
     addiu $2, $2, 32
 
-next2:
     addiu $2, $2, 64
     jr $14
     addiu $2, $2, 128
-    jr 0$
+    jr $0
     addiu $2, $2, 256
 
 .end	main
